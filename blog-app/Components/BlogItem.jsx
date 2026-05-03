4977446b5484d1da35946627dfde478bcd355586
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-03 14:44:21 +0500

    update

--- blog-app/Components/BlogItem.jsx
+++ blog-app/Components/BlogItem.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function BlogItem() {
   return (
-    <div>BlogItem</div>
+    <div className='max-w[330px] sm:max-w-[300px] bg-whute border-black hover:shadow-[-7px_7px_0px_#000000] '>
+      <img src={blog_data[0].image} alt="" width={400} height={400} className='border-b border-black' />
+      <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{blog-data[0].category}</p>
+      <div className='p-5'>
+        <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{blog_data[0].title}</h5>
+      </div>
+
+    </div>
   )
 }
 
--- blog-app/Components/Header.jsx
+++ blog-app/Components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         </p>
 
         {/* Email Form */}
-        <form className='flex justify-between max-w-[800px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]'>
+        <form className='flex justify-between max-w-[500px] hieght-9px scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]'>
           
           <input
             type='email'
