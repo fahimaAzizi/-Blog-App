5b8cb128eb716f4de4e3a4422e69be330af9eef2
Fahima Azizi <fahimaazizi397@gmail.com>
2026-05-04 14:02:31 +0500

    update

--- blog-app/Components/BlogList.jsx
+++ blog-app/Components/BlogList.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { blog_data } from '../assets/blog_data'
+import BlogItem from './BlogItem'
 
-function BlogList() {
+const BlogList = () => {
+
+  const [menu, setMenu] = useState("All")
+
   return (
-    <div>BlogList</div>
+    <div>
+
+      {/* Buttons */}
+      <div className='flex justify-center gap-6 my-10'>
+        <button onClick={() => setMenu("All")} className='bg-black text-white px-4 py-1 rounded'>
+          All
+        </button>
+
+        <button onClick={() => setMenu("Technology")}>
+          Technology
+        </button>
+
+        <button onClick={() => setMenu("Startup")}>
+          Startup
+        </button>
+
+        <button onClick={() => setMenu("Lifestyle")}>
+          Lifestyle
+        </button>
+      </div>
+
+      {/* Blog List */}
+      <div className='flex flex-wrap justify-center gap-8'>
+
+        {blog_data
+          .filter(item => menu === "All" ? true : item.category === menu)
+          .map((item, index) => (
+            <BlogItem
+              key={index}
+              title={item.title}
+              description={item.description}
+              category={item.category}
+              image={item.image}
+            />
+          ))
+        }
+
+      </div>
+
+    </div>
   )
 }
 
